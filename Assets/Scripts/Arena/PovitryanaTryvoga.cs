7edcc195c76decd42da7f09b33c77fc8bb89027e
Denis Zhukovin <pavekzh@gmail.com>
2023-05-24 00:50:57 +0300

    Added light flicker animation

--- Assets/Scripts/Arena/PovitryanaTryvoga.cs
+++ Assets/Scripts/Arena/PovitryanaTryvoga.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Assets.Scripts.Arena
 {
@@ -7,6 +8,22 @@ namespace Assets.Scripts.Arena
     {
         [SerializeField] Light light;
 
+        Color redColor;
+        Sequence animation;
 
+        private void OnDestroy()
+        {
+            animation.Kill();
+        }
+
+        private void Start()
+        {
+            redColor = light.color;
+
+            animation = DOTween.Sequence()
+                .Append(light.DOColor(Color.white, 1).SetEase(Ease.InOutCubic))
+                .Append(light.DOColor(redColor, 1).SetEase(Ease.InOutCubic))
+                .SetLoops(-1);
+        }
     }
 }
